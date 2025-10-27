bb3ad97d7e21ae0b9eaff5e1c3035e823809a4d5
mchidozie-pixel <m.chidozie@alustudent.com>
2025-10-27 12:16:49 -0700

    Handle saving and reading messages from a file using error handling

--- file_manager.py
+++ file_manager.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-def save_message():
-    user_message = input("Enter a message to save: ")
-    with open("user_message.txt", "w") as file:
-        file.write(user_message)
+from helper_functions import create_message
+user_message_file="user_message.txt"
+def save_message(message):
+    with open(user_message_file, "a") as file:
+        file.write(message)
     print("Message saved to user_message.txt ")
 
 def read_message():
     try:
-        with open("user_message.txt", "r") as file:
+        with open(user_message_file, "r") as file:
             message = file.read()
-            print("Saved message:", message)
+            print("Saved message:\n", message)
     
     except FileNotFoundError:
         print("Error: The file 'user_message.txt' was not found.")
