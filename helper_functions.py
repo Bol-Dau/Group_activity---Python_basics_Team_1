0fe2f76e96ac157f1569e6e87d54d1c569b712a1
ndavid-arch <n.david@alustudent.com>
2025-10-27 21:01:00 +0000

    Creating different functions one to validate, to convert to Binary and to print a message and ensuring the file is in order

--- helper_functions.py
+++ helper_functions.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
+
+#Validating User Input
+
 #Check whether the user input is a string and whether is empty
 def validate_input(user_input):
-    if not isinstance(user_input, str):
+    if not isinstance(user_input, str): #check whether the input is a string
         return False
-    elif not user_input.strip():
+    elif not user_input.strip():#Check if the input is empty after stripping of the white space
         return False
     return True
 
+
+#Binary Conversion
+
+#Here we Converted the name and age to binary numbers
 def convert_to_binary(text):
-    if text.isdigit():
-        age_binary = bin(int(text))
+    if text.isdigit(): #Check whether the input is a digit
+        age_binary = bin(int(text)) #Convert the age to an integer and then the integer to a binary display
         return age_binary
     else:
-        for char in text:
-            char = ord(char)
-            name_binary = format(char,'08b')
+        for char in text: #iterating over all characters in the name
+            char = ord(char) #converting the characters in the name to ASCII values
+            name_binary = format(char,'08b') #Converting the ASCII values to binry using the format function and '08b' to ensure that the binary representation is 8 bits long rather than 7
             name_binary = name_binary =+ " "
             return name_binary
 
+
+#Creating the Personalized Messag e
 
 def create_message(name, age, name_binary, age_binary):
     #Combines all details into a full personalized message.
