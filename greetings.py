b736b69f0b72b7b8911e7060a877c29f7d56e557
Monique-creator <m.niyobyose@alustudent.com>
2025-10-27 08:18:05 -0700

    add greetings.py with intro and exit functions

---
+++ greetings.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+#greetings.py
+#This file defines two simple functions:
+#1. show_intro() — prints the welcome message
+#2. show_exit_message() — prints the goodbye message.
+
+def show_intro():
+    #Display a friendly welcome message.
+    print("=================================")
+    print("Welcome to the User Info Program")
+    print("=================================")
+
+
+def show_exit_message():
+    #Display a goodbye message.
+    print("Thank you for using the program. Goodbye!")
+
+
+
